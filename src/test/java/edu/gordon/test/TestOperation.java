cb53c23ddf20f2dbef8ea5c71ca162b976d6eb42
GraciaDufort <GraciaDufort@gmail.com>
2016-02-09 18:05:42 -0500

    Test Deposit Complete

--- src/test/java/edu/gordon/test/TestOperation.java
+++ src/test/java/edu/gordon/test/TestOperation.java
@@ -12,6 +12,7 @@ import edu.gordon.atm.common.Cancelled;
 import edu.gordon.atm.display.Display;
 import edu.gordon.atm.display.Display.ReadInputCallback;
 import edu.gordon.atm.physical.ICashDispenser;
+import edu.gordon.atm.physical.IEnvelopeAcceptor;
 import edu.gordon.atm.physical.INetworkToBank;
 import edu.gordon.atm.physical.IReceiptPrinter;
 import edu.gordon.atm.transaction.Deposit;
@@ -27,7 +28,7 @@ import edu.gordon.banking.Money;
 import edu.gordon.banking.Receipt;
 import edu.gordon.banking.Status;
 
-public class TestTransaction {
+public class TestOperation {
 	private Money argentTotal;
 	private Money argentDisponible;
 	private Balances solde;
@@ -65,8 +66,6 @@ public class TestTransaction {
 	
 	@Test
 	public void performWithdrawal() throws Cancelled, CardRetained{
-	/*	transaction.getState(0);
-		transaction.performTransaction();*/
 		atm.setCustomerConsole(display);
 		atm.setCashDispenser(new ICashDispenser() {
 			
@@ -147,8 +146,105 @@ public class TestTransaction {
     	/*transaction.performTransaction();
 		System.out.println("TEST END");*/
     	assertTrue(transaction.performTransaction());
-		
-		
-		
+
 	}
+	
+	@Test
+	public void performDeposit() throws Cancelled, CardRetained{
+		atm.setCustomerConsole(display);
+		atm.setCashDispenser(new ICashDispenser() {
+			
+			public void setInitialCash(Money initialCash) {}
+			
+			public void dispenseCash(Money amount) {}
+			
+			public boolean checkCashOnHand(Money amount) {
+				
+				return true;
+			}
+		});
+		
+		String[] testMenu = new String[4];
+			testMenu[0]="Withdrawal";
+			testMenu[1]="Deposit";
+			testMenu[2]="Transfer";
+			testMenu[3]="Balance Inquiry";
+			
+		display.setReadInputCallback(new ReadInputCallback(){
+			public String readInputCallbackReceived(int mode, int maxValue) {
+				return "2";
+			}
+		});
+				
+		display.readMenuChoice("TestPrompt", testMenu);
+		
+		//TODO comment
+		System.out.println("makeTransaction WILL BE called");
+		
+		TransactionFactory transactionFactory = new TransactionFactory(atm);
+		
+		transaction  = transactionFactory.makeTransaction(session, carte, 456);System.out.println("yep yep");
+		
+		//TODO comment
+    	System.out.println("B4 assertTrue");
+    	
+    	atm.setNetworkToBank(new INetworkToBank() {
+			
+			public Status sendMessage(Message message, Balances balances) {
+				return new Status(){
+
+					@Override
+					public boolean isSuccess() {
+						return true;
+					}
+
+					@Override
+					public boolean isInvalidPIN() {
+						return false;
+					}
+
+					@Override
+					public String getMessage() {
+						return "Success!";
+					}
+				};
+			}
+			
+			public void openConnection() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void closeConnection() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    	
+    	atm.setReceiptPrinter(new IReceiptPrinter() {
+			
+			public void printReceipt(Receipt receipt) {
+				
+			}
+		});
+    	
+    	atm.setEnvelopeAcceptor(new IEnvelopeAcceptor() {
+			
+			public void acceptEnvelope() throws Cancelled {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    	
+    	display.setReadInputCallback(new ReadInputCallback(){
+			public String readInputCallbackReceived(int mode, int maxValue) {
+				return "1";
+			}
+		});
+    	
+    	assertTrue(transaction.performTransaction());
+
+	}
+	
+
 }
